5c2501dd536fba9089f54e385e5bbe28f009a8ba
samruddhi2592 <er.samruddhi25@gmail.com>
2020-07-31 16:18:39 +0530

    welgreetprops

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import React,{Component} from 'react';
+
 import Greet from './Greet';
-import Welcome from './Welcome';
+// import Welcome from './Welcome';
+// import Hello from './Hello';
 class App extends Component{
-  render()
-{
+  render(){
+const App = () => {
   return (
     <div className="App">
- {/* <Greet/> */}
- <Welcome/>
-    </div>
+     <Greet name="Sam" heroName="superwomen"><p>This is children props</p></Greet>
+     <Greet name="Akhil" heroName="batman"><p>This is children props</p></Greet>
+     <Greet name="Kuku" heroName="superman"><p>This is children props</p>
+    <button>Action</button> 
+     </Greet>
+   </div>
   );
+}
   }
 }
 export default App;
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>Hello World</h1>
-}
+const Greet= props => {
+console.log(props)
+
+return(
+<div>
+    <h1>Hello {props.name} is {props.heroName}</h1>
+{props.children}
+    
+    </div>)}
 export default Greet
