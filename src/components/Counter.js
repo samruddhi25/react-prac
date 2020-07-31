6d670a8522ca8b50a369cdc9de49f1554378d639
samruddhi2592 <er.samruddhi25@gmail.com>
2020-07-31 18:52:54 +0530

    count1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-constructor(props) {
-    super(props);
-    this.state = {
-count : 0
+    constructor(props) {
+        super(props);
+        this.state={
+            count:0
+        }
     }
-}
-increment(){
-    this.setState(
-        {
-count : this.state.count+1 
-    },
-    
-    () => {
-        console.log('Callback Value',this.state.count)
-    }
-    )
-    console.log(this.state)
+    increment(){
+ 
     this.setState((prevState,props)=>(
         {
             count:prevState.count+1
@@ -25,15 +16,21 @@ count : this.state.count+1
     ))
     console.log(this.state.count)
 }
-
-    render() {
-        return (
-            <div>
-                Count - {this.state.count}
-                <button onClick = {() =>this.increment()}> Increment</button>
-            </div>
-        );
-    }
+incrementFive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
 }
 
-export default Counter;
+render() {
+        return (
+            <div className="App">
+<div>Count-{this.state.count} </div> 
+<button onClick={()=>this.incrementFive()}>Increment</button></div>      
+            
+        )
+    }
+}
+export default Counter
