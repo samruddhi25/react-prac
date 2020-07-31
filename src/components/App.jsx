def06cfbd30f91d6bece04c2e027194be83b934e
samruddhi2592 <er.samruddhi25@gmail.com>
2020-07-31 15:20:27 +0530

    welcomeclass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React,{Component} from 'react';
 import Greet from './Greet';
-
+import Welcome from './Welcome';
 class App extends Component{
   render()
 {
   return (
     <div className="App">
  <Greet/>
+ <Welcome/>
     </div>
   );
   }
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from'react';
+import {Component} from 'react'
+class Welcome extends Component{
+render(){
+    <h1>
+      Class Component
+    </h1>
+    
+    
+}
+}
+export default Welcome
