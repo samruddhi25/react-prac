965216e240c0676a7fade5f84be936a125c442d9
samruddhi2592 <er.samruddhi25@gmail.com>
2020-07-31 14:10:25 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+import Hello from './Hello';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      
+<Hello/>           
+
+    </div>
+  )
+}
+
+export default App;
